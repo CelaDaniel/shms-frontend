abc230aca4aca10892fe592620e9da7a17aa4ee7
klarenc <khajderlli@gmail.com>
2023-11-24 15:05:46 +0100

    added error handler interceptor

---
+++ src/app/core/interceptor/error.interceptor.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {
+    HttpInterceptor,
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    constructor(private snackBar: MatSnackBar) {}
+
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            catchError((error: HttpErrorResponse) => {
+                if (!req.url.includes('/auth')) {
+                    this.showSnackBar(error.error.message);
+                }
+                return throwError(error);
+            })
+        );
+    }
+
+    private showSnackBar(message: string): void {
+        this.snackBar.open(message, 'Close', {
+            duration: 5000, // Duration in milliseconds
+            horizontalPosition: 'center',
+            verticalPosition: 'top',
+        });
+    }
+}
